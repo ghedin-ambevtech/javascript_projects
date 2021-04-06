df024bf9dd220f348a111f1525917283f0dd7a3a
Alison Ghedin <alison.ghedin@ambevtech.com.br>
2021-04-05 23:13:43 -0300

    troca de cores

--- background_color/script.js
+++ background_color/script.js
@@ -4,5 +4,16 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color')
 
 btn.addEventListener('click', function(){
-    console.log(document.body);
+  // get number random 0 - 3
+  const randomNumber = getNumberRandom();
+  console.log(randomNumber);
+  // change background color
+  document.body.style.backgroundColor = colors[randomNumber]
+  // change text of color
+  color.textContent = colors[randomNumber]
 })
+
+
+function getNumberRandom(){
+    return Math.floor(Math.random() * colors.length)
+}
